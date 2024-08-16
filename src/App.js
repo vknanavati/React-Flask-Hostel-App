35f7915bb5f54be9a1bbdae7b6fd96d4b49e41bf
vknanavati <vimin.nanavati@gmail.com>
2024-08-16 17:33:58 -0400

    created textfield for user to enter country name which is sent to flask backend, receives JSON object response from flask which is accepted by useState function setResponse and is displayed on frontend site.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, TextField, Button, Typography } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [country, setCountry] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // send the country name to the Flask backend
+    try {
+      const res = await fetch('http://localhost:5000/get-country', {
+        method: 'POST',
+        //informs the server about the format of data being sent
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // actual data being sent to server
+        // converts JSON object country into a JSON string format
+        // '{"country": "Canada"}' is the result of JSON.stringify
+        // This line converts the JavaScript object { country } into a JSON string. In this object, country is the key, and its value is whatever the user typed in the text field.
+        body: JSON.stringify({ country }),
+      });
+
+      // parses the JSON response from the server into a JS object
+      const data = await res.json();
+      //  updates the response state with the message from the server or a default message if no response is provided.
+      // const data = { message: "Received country: France"};
+      setResponse(data.message || "No response");
+    } catch (error) {
+      setResponse("Error: Could not connect to server.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Typography>Hostel Search</Typography>
+      <form>
+        <TextField
+          type="text"
+          placeholder="Enter country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <Button
+          type="submit"
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Submit
+        </Button>
+      </form>
+      <Typography>{response}</Typography>
+    </Container>
   );
 }
 
