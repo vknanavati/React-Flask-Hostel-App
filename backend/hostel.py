a223e827936bb8390965f6f7f7542102fbaff343
vknanavati <vimin.nanavati@gmail.com>
2024-08-16 20:15:23 -0400

    added function to retrieve continent from continent dict based on value of country from get_country function. Added get_cities function to make API call to hostelworld to scrape for cities, city_list from the get_cities function is then returned in get_country function. get_city function is called im get_country function.

--- backend/hostel.py
+++ backend/hostel.py
@@ -1,8 +1,35 @@
+import json
+from urllib.request import urlopen
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
+chrome_options = Options()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("detach", True)
+options.add_experimental_option("useAutomationExtension", False)
+CHROME_DRIVER_PATH = '/usr/local/bin/chromedriver'
+service = Service(executable_path=CHROME_DRIVER_PATH)
+
+browser = webdriver.Chrome(
+    options=options,
+    service=service,
+)
 app = Flask(__name__)
 CORS(app)
+
+
+with open("continent_dict.json", encoding="UTF-8") as country_dict:
+    country_dict = country_dict.read()
+
+country_dict = json.loads(country_dict)
 
 # decorator that defines a route for handling HTTP requests
 # the @app.route decorator specifies which URL paths (routes) should be handled by a particular function # noqa
@@ -22,12 +49,63 @@ def get_country():
     print(f"data: {data}")
     # data.get('country') retrieves the value associated with the key 'country' from the JSON data # noqa
     country = data.get('country')
+    print(f"country from JSON data received from user input: {country}")
+
+    if country:
+        continent = get_continent(country)
+        if continent:
+            city_results = get_cities(country, continent)
+            return jsonify({"country": country, "continent": continent, "cities": city_results}) # noqa
 
     # For demonstration, simply return the received country
     # converts the python data into a JSON formatted string
     # sets the Content-Type header to application/json
     # tells the client that response being returned is in JSON format
-    return jsonify({"message": f"Received country: {country}"})
+    # return jsonify({"message": f"Received country: {country}"})
+
+
+def get_continent(country):
+    # item = value of k:v
+    for item in country_dict.values():
+        # print(f"\nCountries in continent: {item}\n")
+        # element represents the country in the list of countries from dictionary that country variable will be compared against # noqa
+        for element in item:
+            # country is the user's choice
+            if country == element:
+                continent = [k for k, v in country_dict.items() if v == item][0] # noqa
+                print(f"\nContinent of country: {continent}\n")
+                return continent
+    return None
+
+
+city_list = []
+
+
+def get_cities(country, continent):
+    # generates list of cities with hostels in that country
+    url = f"https://www.hostelworld.com/st/hostels/{continent}/{country}/"
+    print(f"\nURL to be opened: {url}\n")
+    html = urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+    soup.prettify()
+
+    browser.get(url)
+    # time.sleep(4)
+
+    results = browser.find_elements(
+        By.XPATH,
+        "//div[@class='average-city-prices-list']/a"
+    )
+
+    for result in results:
+        title = result.get_attribute("title")
+        city_list.append(title)
+    print(f"\nList of cities: {city_list}\n")
+    # print(*city_list, sep="\n")
+    # time.sleep(5)
+    # browser.quit()
+
+    return city_list
 
 
 if __name__ == '__main__':
