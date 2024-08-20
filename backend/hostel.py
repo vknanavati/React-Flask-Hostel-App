ca6bd7acbfb0ee947bb7234a41a891d6aa10bc0a
vknanavati <vimin.nanavati@gmail.com>
2024-08-19 20:31:47 -0400

    added links_city_hostels function and city_hostel_dict function. Added logic to create hostel ratings csv.

--- backend/hostel.py
+++ backend/hostel.py
@@ -1,4 +1,8 @@
 import json
+import time
+from time import sleep
+import requests
+import re
 from urllib.request import urlopen
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -7,6 +11,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import pandas as pd
+from random import randint
 
 chrome_options = Options()
 options = webdriver.ChromeOptions()
@@ -53,6 +59,9 @@ def get_country():
 
     continent = get_continent(country)
     city_results = get_cities(country, continent)
+
+    if not city_results:  # Check if city_results is empty or None
+        city_results = []
     # jsonify is a Flask function that converts the python dictionary into a JSON-formatted string (JSON object) # noqa
     # it also sets the 'Content-Type' header of the response to 'application/json' this tells the client that the content being returned is in JSON format # noqa
     # jsonify returns a Flask response object that contains the JSON data and appropriate headers # noqa
@@ -103,17 +112,165 @@ def get_cities(country, continent):
 @app.route('/get-user-city', methods=['POST'])
 def get_user_city():
     data = request.json
+    #  {'country': 'England', 'continent': 'europe', 'city': 'London'}
     print(f"\nData received from client after user selected city: {data}\n")
+
+    country = data.get('country')
+    continent = data.get('continent')
+    city = data.get('city')
+
+    paginated_list = city_page(continent, country, city)
+    hostels_links = links_city_hostels(paginated_list)
+    city_ratings_dict = city_hostel_dict(hostels_links)
+
+    df = pd.DataFrame(city_ratings_dict)
+    # print(df)
+    df.to_csv("Hostel_City_Ratings.csv")
+    print("\nCSV created! YAY!!\n")
 
     if not data:
         return jsonify({"error": "no data received"}), 400
 
-    city = data.get('city')
     if not city:
         return jsonify({"error": "no city received"}), 400
 
     result = {"city": city}
     return jsonify(result), 200
+
+
+def city_page(continent, country, city):
+    url = f"https://www.hostelworld.com/st/hostels/{continent}/{country}/{city}/" # noqa
+    # replaces any white space with %20
+    url = url.replace(" ", "%20")
+    print(url)
+    html = urlopen(url)
+    # page = requests.get(url, timeout=10)h
+
+    soup = BeautifulSoup(html, "html.parser")
+    soup.prettify()
+
+    browser.get(url)
+    time.sleep(4)
+
+    # def pagination_count():
+    if soup.find("section", {"name": "pagination"}) is not None:
+        print("\nDetected for pagination.\n")
+
+        time.sleep(8)
+        results = browser.find_element(
+            By.XPATH,
+            "//main/div[3]/section",
+        )
+        print(results)
+        raw_string = results.get_attribute("innerText").replace("\n", "")
+        digit_string = re.sub("[^0-9]", "", raw_string)
+        # digit_list = number of pages ex. London has 4 pages worth of hostels
+        # so digit_list = [1, 2, 3, 4]
+        digit_list = [int(page) for page in digit_string]
+
+        print("\nDetermined number of pages.\n")
+        print(f"\ndigit_list is: {digit_list}\n")
+
+        time.sleep(5)
+
+        # url_list initially creates template hostel link for each element in digit list # noqa
+
+        url_list = [url for count in enumerate(digit_list)]
+
+        # print([url for count in range(list_len)])
+        # page number syntax is added to [1:]
+        # url_list: [
+        #   'https://www.hostelworld.com/st/hostels/europe/england/london/',
+        #   'https://www.hostelworld.com/st/hostels/europe/england/london/p/2/',
+        #   'https://www.hostelworld.com/st/hostels/europe/england/london/p/3/',
+        #   'https://www.hostelworld.com/st/hostels/europe/england/london/p/4/'
+        # ]
+        i = 1
+        for index, dummy in enumerate(url_list):
+            i = i + 1
+            url_list[index] = url_list[index] + "/p/" + str(i) + "/"
+
+        print(f"Paginated url list: {url_list}")
+        return url_list
+    else:
+        print("\nThis city has only one page worth of hostels.\n")
+        url_list = [url]
+        print(f"Single url link: {url_list}")
+        return url_list
+
+
+links_list = []
+
+
+def links_city_hostels(paginated_list):
+
+    for index, dummy in enumerate(paginated_list):
+        page = requests.get(paginated_list[index], timeout=10)
+        soup = BeautifulSoup(page.content, "html.parser")
+        soup.prettify()
+
+        link_elements = soup.find_all("div", class_="property-listing-cards")
+        # scrape each page for links for each hostel listed
+        for link in link_elements:
+            results = link.find_all("a")
+            for result in results:
+                link_url = result["href"]
+                links_list.append(link_url)
+        browser.quit()
+
+    url_count = len(links_list)
+    print(f"\nlinks_list = {links_list}\n")
+    print(f"\nLength of list: {url_count}")
+    return url_count
+
+
+def city_hostel_dict(hostels_links):
+    name_list = []
+    composite_hostel_scores = []
+
+    for url in range(0, hostels_links):
+        page = requests.get(links_list[url], timeout=10)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        soup.prettify()
+
+        hostel_name = soup.find("h1").text.strip()
+        name_list.append(hostel_name)
+
+        parent_divs = soup.find_all('div', class_='rating-label-score-wrapper')
+
+        specific_scores = [
+            parent_div.find('div', class_='rating-score').text.strip() for
+            parent_div in parent_divs
+        ]
+
+        composite_hostel_scores.append(specific_scores)
+
+        lists_to_join = zip(name_list, composite_hostel_scores)
+        specific_ratings = list(lists_to_join)
+        # print(f"\nSpecific ratings: {specific_ratings}\n")
+        ratings_dict = dict(specific_ratings)
+
+        dict_length = len(ratings_dict)
+
+        seconds = randint(2, 10)
+        sleep(seconds)
+
+        print(f"\nProgress: {dict_length}/{hostels_links} ")
+        print(f"I waited {seconds} seconds\n")
+
+        sleep(randint(2, 10))
+
+    no_ratings_dict = {key: value for (key, value) in ratings_dict.items() if not value} # noqa
+
+    ratings_dict = {key: value for (key, value) in ratings_dict.items() if value} # noqa
+
+    no_rating_list = list(no_ratings_dict.keys())
+    no_rating_string = ", ".join([str(elem) for elem in no_rating_list])
+
+    print(f"\nComplete Dictionary: {ratings_dict}\n")
+    print(f"Unrated hostels: {no_rating_string}")
+    return ratings_dict
 
 
 if __name__ == '__main__':
